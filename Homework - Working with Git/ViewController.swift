339a764d95d9f5b2ed99e3d9dc3e46d5f46433a9
Alexandra <AlexandraSevost@icloud.com>
2022-07-31 18:48:52 +0300

    Clean cpmments

--- Homework - Working with Git/ViewController.swift
+++ Homework - Working with Git/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  Homework - Working with Git
-//
 //  Created by Александра  Севостьянова on 31.07.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
